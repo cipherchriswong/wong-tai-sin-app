3deca1840613f8f0b1e499f8faa2c6b7ed69066f
cipherchriswong <cipherchrise@gmail.com>
2025-04-12 09:09:32 +0800

    Delete qian100_complete.js

--- qian100_complete.js
+++
@@ -1,6 +0,0 @@
-
-const fortunes = [
-  { id: 1, level: "上籤", content: "風送桃花過水流", meaning: "吉運將至，宜把握機會。" },
-  { id: 13, level: "中籤", content: "這是第13籤的詩句內容，富有啟發與智慧。", meaning: "如果循環謹慎行事，隨緣而安。" },
-  { id: 53, level: "下籤", content: "這是第53籤的詩句內容，富有啟發與智慧。", meaning: "如果循環謹慎行事，隨緣而安。" }
-];
